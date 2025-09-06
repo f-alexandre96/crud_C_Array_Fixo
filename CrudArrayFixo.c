40a18dffd779c381b3ba52c31d28d968b3eaa179
Fernando Alexandre da Silva <fernando17034@gmail.com>
2025-09-06 19:24:16 -0300

    CRUD com array fixo em C (sem alocação dinâmica)

---
+++ CrudArrayFixo.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+typedef struct{
+	int id;
+	char nome[50];
+	int idade;
+}Pessoa;
+
+void criarPessoa(Pessoa pessoas[],int quantidade){
+	
+}
+void listarPessoa(){
+	
+}
+void atualizarPessoa(){
+	
+}
+void deletarPessoa(){
+	
+}
+
+int main(){
+	setlocale(LC_ALL,"portuguese");
+
+
+return 0;
+}
